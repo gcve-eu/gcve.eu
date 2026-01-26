0a9b6d6521b76cb242f89f18021f159fbe5a2098
Alexandre Dulaunoy <a@foo.be>
2026-01-26 21:15:27 +0100

    chg: [PDF] BCP PDF generation improved

--- content/bcp/build.sh
+++ content/bcp/build.sh
@@ -1,6 +1,6 @@
-for file in *.md;
+for file in gcve-bcp*.md;
 do
-    pandoc ${file} --columns=10  --pdf-engine=xelatex -V logo="gcve.png" -V book=false -V titlepage-rule-color="2e58e3" -V titlepage=true -V colorlinks=true -V linkcolor=blue  -V urlcolor=red  -V toccolor=gray --toc-depth=6 -V number-offset=2,6 --template eisvogel  -o ${file%%.*}.pdf  -V toc-own-page=true -V first-chapter=1 -V titlepage-logo="gcve.png" -V listings-no-page-break=true -V author="GCVE.eu" --toc --syntax-highlighting=idiomatic 
+    pandoc ${file} --columns=10  --pdf-engine=xelatex -V logo="gcve.png" -V book=false -V titlepage-rule-color="2e58e3" -V titlepage=true -V colorlinks=true -V linkcolor=blue  -V urlcolor=red  -V toccolor=gray --toc-depth=6 -V number-offset=2,6 --template eisvogel  -o ${file%%.*}.pdf  -V toc-own-page=true -H tex.command -V first-chapter=1 -V titlepage-logo="gcve.png" -V listings-no-page-break=true -V author="GCVE.eu" --toc --syntax-highlighting=idiomatic 
     mv *.pdf ../../static/files/bcp
 done 
 
